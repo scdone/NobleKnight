13ee73aac7abab6105992a399643cc15834aff98
Stetson Done <scdone@gmail.com>
2022-01-19 13:11:39 -0700

    added css for previous games screen

--- src/components/playerHistory.js
+++ src/components/playerHistory.js
@@ -3,7 +3,13 @@ function PlayerHistory() {
     return (
 
         <section className='player-history-section'>
-            <h1>here you can see your past play-throughs</h1>
+            <h1 ID="previous-games-title">previous games</h1>
+            <container id="previous-games-container">
+                <div>last saved game</div>
+                <div>last last saved game</div>
+                <div>this is an example but it will pull data from player db</div>
+            </container>
+            <button id="start-new-game-button">Start New Game</button>
         </section>
 
     )
