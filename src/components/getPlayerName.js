6aec31994358749cbd087691a0897c0399cf418f
Stetson Done <scdone@gmail.com>
2022-01-19 14:38:22 -0700

    added css for getplayername

--- src/components/getPlayerName.js
+++ src/components/getPlayerName.js
@@ -2,7 +2,13 @@ function GetPlayerName() {
 
     return (
         <section className='get-player-name-section'>
-            <h1>enter thy nameth page</h1>
+            <div id="nameth-door-img-div">
+            <h1 id="nameth">Halt! Before we go further... <br/>What is thy nameth?</h1>
+            <img id="doors-img" src={require('../fonts/doors.png')} />
+            </div> 
+            <form id="get-player-name-form">
+                <input type="text" id="get-name-input" name="playerName"/>
+            </form>
         </section>
     )
 }
