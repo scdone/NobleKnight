b713d1c76b00499b0d33abdab4e4e2855f2290d4
Stetson Done <scdone@gmail.com>
2022-01-19 10:37:37 -0700

    styled CSS for login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,29 @@
 function Login() {
 
     return (
-
+        <div class="row">
         <section className='login-section'>
-            <h1>This is my login page. Or Play as Guest</h1>
+                <div class="login-form column">
+                    <h2>Login</h2>
+                <form className="user-login">
+                    <label for="username"><b>username</b></label>
+                    <br/>
+                    <input type="text" placeholder="enter username" name="uname" id="username-input" required />
+                    <br/>
+                    <label for="password"><b>password</b></label>
+                    <br/>
+                    <input type="text" placeholder="enter password" name="passw" id="password-input" required />
+                    <br/>
+                    <input type="submit" id="login-submit-button"/>
+                    <br/>
+                </form>
+                </div>
+                <div className="play-as-guest column">
+                    <h2 className="play-guest">Play as Guest</h2>
+                    <button className="enter-welcome-btn">ENTER</button>
+                </div>
         </section>
+        </div>
 
     )
 }
