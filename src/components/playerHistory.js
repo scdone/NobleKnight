8852f268c0eff7581180fab4f1afff66b06f70ae
Stetson Done <scdone@gmail.com>
2022-01-19 13:29:55 -0700

    added css for previous game page

--- src/components/playerHistory.js
+++ src/components/playerHistory.js
@@ -5,9 +5,12 @@ function PlayerHistory() {
         <section className='player-history-section'>
             <h1 ID="previous-games-title">previous games</h1>
             <container id="previous-games-container">
-                <div>last saved game</div>
-                <div>last last saved game</div>
-                <div>this is an example but it will pull data from player db</div>
+                <div id="most-recent-game">Date: 1/19/2022 / KnightName: Anubis / Score: 25 / Ranking:5</div>
+                <br/>
+                <div id="next-recent-game">Date: 1/12/2022 / KnightName: Maddox / Score: 17 / Ranking:12</div>
+                <br/>
+                <div id="third-recent-game">Date: 1/01/2022 / KnightName: Heathen / Score: -7 / Ranking:69</div>
+                <br/>
             </container>
             <button id="start-new-game-button">Start New Game</button>
         </section>
