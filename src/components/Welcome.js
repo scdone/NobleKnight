c062e81a5b1d785d299bc9c7b0bc2bdee4e8db83
Stetson Done <scdone@gmail.com>
2022-01-19 20:02:25 -0700

    added nav to enter button on homepage

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,10 +1,19 @@
-function Welcome(props) {
+import { useNavigate } from 'react-router-dom'
+
+
+function Welcome() {
+
+    let navigate = useNavigate()
+
+    const handleClickEnterGame =  () => {
+        navigate('login')
+    }
 
     return (
         <section className="welcome-box">
             <h1 className="h1">Noble knight, thou hast a problem!</h1> 
             <h1 className="h1" id="h1-2">Enter if ye dare...</h1>
-            <button className="enter-welcome-btn" onClick={props.enterGame}>ENTER</button>
+            <button className="enter-welcome-btn" onClick={handleClickEnterGame}>ENTER</button>
             <h4 className="author-tagline">A text-based adventure <br/> by Stetson Done</h4>
         </section>
     )
