0e31aae30c8f7fdbd3ce4508161cc4fb5cd752cb
Stetson Done <scdone@gmail.com>
2022-01-26 10:55:20 -0700

    troubleshooting WithRouter react dom v6

--- src/components/WithRouter.js
+++ src/components/WithRouter.js
@@ -1,18 +1,23 @@
-import { 
+import {
+    useLocation,
     useNavigate,
- } from "react-router-dom";
+    useParams
+  } from "react-router-dom";
+  
+  function WithRouter(Component) {
+    function ComponentWithRouterProp(props) {
+      let location = useLocation();
+      let navigate = useNavigate();
+      let params = useParams();
+      return (
+        <Component
+          {...props}
+          router={{ location, navigate, params }}
+        />
+      );
+    }
+  
+    return ComponentWithRouterProp;
+  }
 
-export const WithRouter = (Component)=>{
-    const Wrapper = (props)=>{
-        const history = useNavigate();
-
-        return(
-            <Component
-            history = {history}
-            {...props}
-            />
-        );
-    };
-
-    return Wrapper;
-}
+  export default WithRouter
