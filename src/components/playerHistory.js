2d225df5bd07d5e140559205b9cd16e1efbfbfb0
Stetson Done <scdone@gmail.com>
2022-01-26 12:31:14 -0700

    corrected import statement

--- src/components/playerHistory.js
+++ src/components/playerHistory.js
@@ -1,5 +1,5 @@
-import { useHistory } from 'react-router-dom'
-import React, { useEffect, useState, withRouter } from 'react'
+import { useHistory, withRouter } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 
